f9dce6824711068b2a64f7e5793499cbc2573f76
Gabriel Serralha Miró <gsmiro@gmail.com>
2015-10-16 15:43:32 -0300

    Integrated queue to the scraping logic. Need to figure how to add maxRetries for same function

--- 800w.js
+++ 800w.js
@@ -8,10 +8,11 @@ var http = require('http');
 var https = require('https');
 
 var mongourl = 'mongodb://localhost:27017/800w';
+var dict = 'abcdeèfgijklmnoòprstuvwyz';
 
-function queue(msr){
+function queue(msr,end){
   var _q = [];
-  this.state = 'paused'
+  this.state = 'running'
   var _self = this;
   var _count = 0;
   var _srs = msr;
@@ -31,21 +32,24 @@ function queue(msr){
 
   this.enqueue = function(f){
     _q.push(f);
+    console.log('enqueue '+_q.length + ' state:' + _self.state)
+    if(_self.state == 'running')run();
   }
 
   function run(){
-    if(_self.state == 'running' && _q.length && _count == 0){
-      console.log('running with srs:'+_srs +' and length:'+_q.length);
-      var err = 0;
-      for(_count = 0 ; _count < _srs && _q.length;_count++){
+    console.log('state:'+_self.state+' srs:'+_srs +' length:'+_q.length + ' count:' + _count );
+    if(_self.state == 'running' && _q.length && _count < _srs){
+      console.log('Adding to queue '+ (_srs - _count));
+      var _fail = 0;
+      for(;_count < _srs && _q.length;_count++){
         (function call(f){
           f(function _succ(){
             _count--;
             if(_count == 0){
-              _srs = _srs - err;
+              _srs = _srs - _fail;
               if(_srs < 1){
                 _srs = 1;
-              }else if(err == 0){
+              }else if(_fail == 0){
                 _srs++;
                 if(_srs > msr)_srs = msr;
               }
@@ -53,13 +57,13 @@ function queue(msr){
             }
           },function _err(){
             _count--;
-            err++;
+            _fail++;
             _self.enqueue(f);
             if(_count == 0){
-              _srs = _srs - err;
+              _srs = _srs - _fail;
               if(_srs < 1){
                 _srs = 1;
-              }else if(err == 0){
+              }else if(_fail == 0){
                 _srs++;
                 if(_srs > msr)_srs = msr;
               }
@@ -69,7 +73,7 @@ function queue(msr){
         })(_q.pop());
       }
     }else if(_q.length == 0){
-      _self.pause();
+      end();
     }
   }
 
@@ -79,36 +83,14 @@ function connect(cb){
   mongo.connect(mongourl,function(err,db){
     if(err != null) throw err;
     if(db != null){
-      try{
-        cb(db,function(err,r){
+        db.on('error',function(e){
+          console.log('Erro de Banco')
+        })
+        cb(db,function(){
           db.close();
         });
-      }catch(error){
-        try{
-          console.log('Closing connection!');
-          db.close();
-        }catch(connerr){
-
-        }
-      }
     }
   })
-}
-
-function parseText(text,db){
-    var words = text.split(" ");
-    for(var i = 0;i< words.length;i++){
-      var w = words[i].toLowerCase();
-      if(w.length)
-        db.collection('words').findOneAndUpdate(
-          {'w':w},
-          {$inc :{ 'count': 1}},
-          {upsert:true},
-          function(err,r){
-
-          }
-        );
-    }
 }
 
 function request(base,url,succ,err,redir,maxRedir){
@@ -122,27 +104,48 @@ function request(base,url,succ,err,redir,maxRedir){
   try{
     proto.get(url,function(res){
       if(res.statusCode === 200)succ(res);
-      else if(res.statusCode === 302 && res.headers && res.headers.location && redir < maxRedir){
+      else if(res.statusCode > 300 && res.statusCode < 400 && res.headers && res.headers.location && redir < maxRedir){
         if(res.headers.location.indexOf('/') == 0){
           var parsed = urlp.parse(base);
-          request(base,parsed.protocol + '//' + parsed.host + res.headers.location,succ,redir + 1,maxRedir);
+          request(base,parsed.protocol + '//' + parsed.host + res.headers.location,succ,err,redir + 1,maxRedir);
         }else if(res.headers.location.indexOf(base) == 0)
-          request(base,res.headers.location,succ,redir + 1,maxRedir);
+          request(base,res.headers.location,succ,err,redir + 1,maxRedir);
       }
     }).on('error',function(e){
       console.log(e);
+      succ();
     });
-  }catch(error){
-    console.log(error);
+  }catch(exc){
+    console.log(exc);
+    succ();
   }
+
 }
 
-function scrape(base,db,q,cb,url){
+function parseText(text,stats,dict){
+    stats = stats || {}
+    var words = text.split(" ");
+    var rg = new RegExp('['+dict+']+','g');
+    for(let w of words){
+      var matches =  w.toLowerCase().match(rg);
+      if(matches)
+        for(let m of matches)
+          stats[m] = stats.hasOwnProperty(m)?stats[m] + 1:1;
+    }
+    return stats;
+}
+
+function scrape(base,db,q,url,dict,s,e){
   console.log('scrape ' + url);
   var urls = []
+  var stats = {};
   request(base,url,function(res){
+    if(res == null){
+      s();
+      return;
+    }
     var parser = new htmlparser.Parser({
-      ontext: function(text){parseText(text,db)},
+      ontext: function(text){stats = parseText(text,stats,dict)},
       onopentag: function(tag,attr){
         if(attr.hasOwnProperty('href')){
             var purl = urlp.resolve(base,attr.href);
@@ -155,24 +158,34 @@ function scrape(base,db,q,cb,url){
     res.on('data',function(data){parser.write(data); }).on('end',function(){
       parser.end();
       console.log('parsed ' + url);
-      console.log(urls);
-      db.collection('urls').updateOne({'url':url},{$set: {'urls':urls}},{upsert:true});
-      for(var i = 0; i < urls.length; i++){
-        check(base,db,q,cb,urls[i]);
-      }
+      db.collection('urls').updateOne({'url':url},{$set: {'urls':urls}},{upsert:true},function(err,r){
+        if(err)throw err;
+        let blk = db.collection('words').initializeOrderedBulkOp();
+        for(let w in stats){
+          blk.find({'w':w}).upsert().updateOne({$inc:{'count':stats[w]}})
+        }
+        blk.execute(function(err,r){
+          if(err) throw err;
+          s();
+          for(let url of urls){
+            check(base,db,q,url);
+          }
+        })
+
+      });
+
     })
-  });
+  },e);
 }
 
-function check(base,db,q,cb,url){
-  if(typeof url == 'undefined'){
-    url = base
-  }
+function check(base,db,q,url,dict){
+  url = url || base;
   console.log('check ' + url + ' with base ' + base);
   db.collection('urls').find({'url':url}).limit(1).each(function(err,o){
     if(err) throw err;
     if(o != null && o.urls){
       db.collection('urls').find({'url':{$in : o.urls}},function(err,urls){
+        if(err) throw err;
         if(urls.length == o.urls.length)cb(db);
         else {
           for(let url of o.urls){
@@ -185,7 +198,7 @@ function check(base,db,q,cb,url){
             }
             if(!found) q.enqueue(
               function(succ,err){
-                scrape(base,db,q,cb,url)
+                scrape(base,db,q,url,dict,succ,err)
               }
             );
           }
@@ -194,37 +207,36 @@ function check(base,db,q,cb,url){
     }else {
       q.enqueue(
         function(succ,err){
-          scrape(base,db,q,cb,url)
+          scrape(base,db,q,url,dict,succ,err)
         }
       );
     }
   });
 }
 
-/*
+
 connect(function(db,close){
-  check('http://wol.jw.org/ht',db,new queue(100),function(db){
+  check('http://wol.jw.org/ht',db,new queue(100,function(){
       db.collection('words').find().sort([['count', -1]]).limit(800)
         .toArray(function(err,docs){
           console.log(docs);
           close();
         });
-  });
+      }),null,dict);
+
 });
-*/
-var q = new queue(5);
-for(let j = 0;j< 20;j++){
+/*
+var q = new queue(5,function(){
+  console.log('Finish');
+});
+q.pause();
+for(let j = 0;j< 10;j++){
   (function a(j){
     q.enqueue(function(s,e){
       console.log('get '+j)
-      http.get('http://www.jw.org',function(res){
-        console.log(j + ' '+res.statusCode);
-        s();
-      }).on('error',function(err){
-        console.log(j + ' err');
-        e();
-      });
+      request('http://www.jw.org','http://www.jw.org'+(j % 3 == 0?j:'') ,s,e);
     })
   })(j);
 }
 q.resume();
+*/
