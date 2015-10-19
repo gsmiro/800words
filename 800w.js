49af00fe62fb91985bf2036cfc8b05cc6490b290
Gabriel Seralha Miró <gsmiro@gmail.com>
2015-10-19 08:24:42 -0200

    FIxed some queue bugs and added a stop criteria do the queue. The queue does not start on enqueuing anymore

--- 800w.js
+++ 800w.js
@@ -16,7 +16,7 @@ function queue(msr,end){
   var _q = [];
   this.state = 'running'
   var _self = this;
-  var _count = 0;
+  var _pending = 0;
   var _fail = 0;
   var _srs = msr;
   this.stop = function stop_queue(){
@@ -30,25 +30,22 @@ function queue(msr,end){
 
   this.resume = function resume_queue(){
     _self.state = 'running';
-
+    logger.info('resume');
     return run();
   }
 
   this.enqueue = function(f){
     _q.push(f);
-    if(_self.state == 'running'){
-      return run();
-    }
   }
 
   function run(){
-    if(_self.state == 'running' && _q.length && _count < _srs){
-      logger.info('state:'+_self.state+' srs:'+_srs +' length:'+_q.length + ' count:' + _count );
-      for(;_count < _srs && _q.length;_count++){
+    logger.info(`state:${_self.state} srs:${_srs} length:'${_q.length } pending: ${ _pending}` );
+    if(_self.state == 'running' && _q.length && _pending <= _srs){
+      for(;_pending < _srs && _q.length;_pending++){
         (function call(f){
           f(function _succ(){
-            _count--;
-            if(_count == 0){
+            _pending--;
+            if(_pending == 0){
               _srs = _srs - _fail;
               if(_srs < 1){
                 _srs = 1;
@@ -56,14 +53,13 @@ function queue(msr,end){
                 _srs++;
                 if(_srs > msr)_srs = msr;
               }
-
               return run();
             }
-          },function _err(){
-            _count--;
+          },function _err(requeue){
             _fail++;
-            _self.enqueue(f);
-            if(_count == 0){
+            if(requeue)_self.enqueue(f);
+            _pending--;
+            if(_pending == 0){
               _srs = _srs - _fail;
               if(_srs < 1){
                 _srs = 1;
@@ -77,7 +73,8 @@ function queue(msr,end){
           })
         })(_q.pop());
       }return true;
-    }
+    } else if(_q.length == 0 && _pending == 0)end();
+    else logger.info(`${ _self.state} ${_q.length} ${_pending}`);
     return false;
   }
 
@@ -98,36 +95,50 @@ function connect(cb){
   })
 }
 
-function request(base,url,succ,err,redir,maxRedir){
+function request(base,url,succ,err,maxRetry,maxRedir){
   err = typeof err == 'function' && err || function(res){
     throw res.statusCode + ' ' + res.statusMessage ;
   }
-  logger.info('request '+ url);
+  maxRetry = Number(maxRetry) || 10;
+  var retry = Number(retry) || 0;
   maxRedir = Number(maxRedir) || 10;
-  redir = Number(redir) || 0;
-  var proto = url.match(/https:.*/)?https:http;
-  proto.get(url,function(res){
-    if(res.statusCode === 200)succ(res);
-    else if(res.statusCode > 300 && res.statusCode < 400 && res.headers && res.headers.location && redir < maxRedir){
-      if(res.headers.location.indexOf('/') == 0){
-        var parsed = urlp.parse(base);
-        request(base,parsed.protocol + '//' + parsed.host + res.headers.location,succ,err,redir + 1,maxRedir);
-      }else if(res.headers.location.indexOf(base) == 0)
-        request(base,res.headers.location,succ,err,redir + 1,maxRedir);
-    }
-  }).on('error',function(e){
-    logger.warn(e);
+  var redir = Number(redir) || 0;
 
-    if(e.code == "ECONNRESET")
-      err();
-    else {
-      succ();
-    }
-  });
+  function _r(base,url){
+    var proto = url.match(/https:.*/)?https:http;
+    proto.get(url,function(res){
+      if(res.statusCode === 200)succ(res);
+      else if(res.statusCode > 300
+        && res.statusCode < 400 && res.headers && res.headers.location
+        && redir < maxRedir && retry < maxRetry){
+
+        redir++
+        retry = 0;
+        var rurl = urlp.resolve(base,res.headers.location);
+        if(rurl.indexOf(base) == 0){
+          logger.info('redirect '+ url + ' to ' + rurl + ' redir:'+ redir + ' maxRedir:'+maxRedir + ' retry:'+retry +' maxRetry:'+maxRetry);
+          _r(base,rurl);
+        }
+      } else if(maxRedir == redir){
+        err('MAX_REDIR',redir,retry);
+      } else if(maxRetry == retry){
+        err('MAX_RETRY',redir,retry);
+      } else err(res,redir,retry);
+    }).on('error',function(e){
+      if(e.code == 'ECONNRESET' && retry < maxRetry){
+        retry++;
+        logger.info('retry redir:'+ redir + ' maxRedir:'+maxRedir + ' retry:'+retry +' maxRetry:'+maxRetry);
+        _r(base,url);
+      }else err(e,redir,retry);
+    });
+  }
+  logger.info('request '+ url +' redir:'+ redir + ' maxRedir:'+maxRedir + ' retry:'+retry +' maxRetry:'+maxRetry);
+  _r(base,url);
+
 }
 
-function scrape(base,db,q,url,abc,s,e,end){
-  logger.info('scrape ' + url);
+function scrape(base,db,q,url,abc,s,e){
+  logger.info(`scrape ${base} ${url}`);
   var urls = []
   var stats = {};
   request(base,url,function(res){
@@ -137,90 +148,110 @@ function scrape(base,db,q,url,abc,s,e,end){
     }
     var parser = new htmlparser.Parser({
       ontext: function parseText(text){
+        if(this.parseText){
           var words = text.split(" ");
-          var rg = new RegExp('['+abc+']+','g');
+          var rg = new RegExp(`^[${abc}]+$`,'g');
           for(let w of words){
             var matches =  w.toLowerCase().match(rg);
             if(matches)
               for(let m of matches)
                 stats[m] = stats.hasOwnProperty(m)?stats[m] + 1:1;
           }
-          return stats;
+        }
+        return stats;
       },
       onopentag: function(tag,attr){
         if(attr.hasOwnProperty('href')){
+            this.parseText = false;
             var purl = urlp.resolve(base,attr.href);
             if(purl.indexOf(base) == 0)
               urls = urls.concat(purl);
-        }
+        }else this.parseText = true;
       }
     }, {decodeEntities: true});
 
     res.on('data',function(data){parser.write(data); }).on('end',function(){
       parser.end();
-      logger.info('parsed ' + url);
+      logger.info(`parsed ${base} ${url}`);
       db.collection('urls').updateOne({'url':url},{$set: {'urls':urls}},{upsert:true},function(err,r){
-        if(err){
-          console.log(e)
-          return e();
-        }
+        if(err) throw err;
+        logger.info(`updated ${base} ${url}`);
         let blk = db.collection('words').initializeOrderedBulkOp();
+        var exec = false;
         for(let w in stats){
           blk.find({'w':w}).upsert().updateOne({$inc:{'count':stats[w]}})
+          exec = true;
         }
-        blk.execute(function(err,r){
-          if(err){
-            console.log(e)
-            return e();
-          }
-
-          check(base,db,q,urls,end,abc);
-          s();
-        })
-
+        if(exec)
+          blk.execute(function(err,r){
+            if(err) throw err;
+            logger.info(`upsert ${base} ${url.length}`);
+            check(base,db,q,abc,urls,s);
+          })
       });
 
     })
-  },e);
+  },function(err){
+    e(false)
+  });
 }
 
-function check(base,db,q,urls,end,abc){
+function check(base,db,q,abc,urls,after){
   urls = urls || [base];
-  db.collection('urls').find({'url':{$in : urls}}).each(function(err,furl){
+  db.collection('urls').find({'url':{$in : urls}}).each(function(err,o){
     if(err) throw err;
+    var furl = o && o.url
     if(furl != null){
-      urls.splice(urls.indexOf(furl),1);
+      let idx =  urls.indexOf(furl);
+      if(idx >= 0){
+        urls.splice(urls.indexOf(furl),1);
+      }
     } else {
-      for(let url of urls)
+      for(let url of urls){
+        //logger.info(`check ${base} ${url} ${urls.length}`);
         q.enqueue(
           function(succ,err){
-            scrape(base,db,q,url,abc,succ,err,end)
+            scrape(base,db,q,url,abc,succ,err)
           }
         );
+      }
+      if(typeof after == 'function'){
+        logger.info(`call after ${base} ${urls.length}`);
+        after();
+      }
+      q.resume();
     }
   });
 }
 
 
 connect(function(db,close){
-  check('http://wol.jw.org/ht',db,new queue(100),null,function(){
+  check('http://wol.jw.org/ht',db,new queue(100,function(){
       db.collection('words').find().sort({'count': -1}).limit(800)
         .toArray(function(err,docs){
+          if(err) throw err;
           logger.info(arguments);
           close();
         });
-      },abc);
+      }),abc);
 });
-/*
-var q = new queue(5,function(){
+
+
+/*var q = new queue(5,function(){
   logger.info('Finish');
 });
-q.pause();
 for(let j = 0;j< 10;j++){
   (function a(j){
     q.enqueue(function(s,e){
       logger.info('get '+j)
-      request('http://www.jw.org','http://www.jw.org'+(j % 3 == 0?j:'') ,s,e);
+      var url = 'http://www.jw.org'+(j % 3 == 0?j:'')
+      request('http://www.jw.org',url ,function(res){
+        logger.info('parsed '+ url);
+        s();
+      },function(err){
+        logger.info(err);
+        e(false);
+      });
     })
   })(j);
 }
